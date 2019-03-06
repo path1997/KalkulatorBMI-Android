453aff87df58d0c0a4a7d4352652e8e775a63a2e
path1997 <path1997@users.noreply.github.com>
2019-03-06 17:50:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kalkulatorbmi/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.kalkulatorbmi;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public Button buttonOK;
+    protected EditText etMasa;
+    public EditText etWzrost;
+    public TextView etBMI;
+    private String smasa;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonOK= (Button) findViewById(R.id.buttonOK);
+        buttonOK.setOnClickListener(this);
+        etMasa= (EditText) findViewById(R.id.etMasa);
+        etWzrost= (EditText) findViewById(R.id.etWzrost);
+        etBMI=(TextView) findViewById(R.id.etBMI);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String smasa= etMasa.getText().toString().trim();
+        String swzrost= etWzrost.getText().toString().trim();
+        //String wynik=smasa+swzrost;
+
+        if(smasa.isEmpty() || swzrost.isEmpty()){
+            etBMI.setText("Nieprawidłowe dane");
+        } else {
+            double masa=Double.parseDouble(smasa);
+            double wzrost=Double.parseDouble(swzrost);
+            double wzrostM=wzrost/100;
+            double bmi=masa/wzrostM/wzrostM;
+            bmi=round(bmi,2);
+            String komunikat="";
+            if(bmi<18.49){
+                komunikat="Niedowaga!!!";
+            }
+            else if(bmi>18.5 && bmi<24.99){
+                komunikat="Waga prawidłowa";
+            } else if(bmi>25){
+                komunikat="Nadwaga!!!";
+            }
+
+
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(buttonOK.getWindowToken(), InputMethodManager.RESULT_UNCHANGED_SHOWN);
+            String wynik = String.valueOf(bmi) + "  " + komunikat;
+            etBMI.setText(wynik);
+        }
+
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+}
